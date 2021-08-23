74716156568108fe29612021195adff5b1e44363
kamlesh <kamleshwagh93@gmail.com>
2021-08-23 10:25:25 +0530

    first commit

---
+++ src/test/java/frameworkbuild/RestApiTest.java
@@ -0,0 +1,23 @@
+package frameworkbuild;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+
+	
+	@Test
+	public void postjira()
+	{
+		System.out.println("postjira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deletetwiter");
+	}
+	
+	
+	
+	
+}
---
+++ src/test/java/frameworkbuild/SeliniumTest.java
@@ -0,0 +1,21 @@
+package frameworkbuild;
+
+import org.testng.annotations.Test;
+
+public class SeliniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("Browserautomation");
+	}
+	
+	@Test
+	public void elementUi()
+	{
+		System.out.println("elementUI");
+	}
+	
+	
+
+}
